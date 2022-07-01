51e4017a71fa7de8559c9534b2078de11c818ca1
Jeff <jeffcrockett03@gmail.com>
2022-07-01 15:41:40 -0400

    added Table import statement to db.py

--- Table.py
+++ Table.py
@@ -1,10 +1,23 @@
+#from Table import Table
+#>>> from Table import Table as t
+
 class Table:
     def __init__(self):
         self.rows = []
         self.cols = []
+        self.cells = []
+        self.html = ''
 
     def rows(self):
         return self.rows
 
     def cols(self):
+        return self.cols
+
+    def add_row(self, row):
+        self.rows.append(row)
+        return self.rows
+
+    def add_col(self, col):
+        self.cols.append(col)
         return self.cols
