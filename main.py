a54e5cfb44c11cfbe4efb7c987d18ae2aed1cbf6
Jeff <jeffcrockett03@gmail.com>
2022-07-03 12:44:32 -0400

    updated main.py

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ f = open('db.txt', 'r')
 rows = f.readlines()
 t = T()
 top_row = t.add_row(rows).rows[0]
-for cell in top_row:
+for cell in t.rows[0]:
     c = C(parent=top_row)
+    print(c)
+    c.add_content(cell)
+    print(c.parent)
+    # t.rows[0].cells.append(c)
+    print(type(t.rows[0]))
+    print("top_row.cells is", cell)
+
+print('top_row is', top_row)
+
+print('rows are', t.rows)
 
 # for row in t.rows.split():
 #     print(row)
