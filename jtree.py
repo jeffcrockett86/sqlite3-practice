35abcfa9a5e7cba2d7c42fdee1f488813a3c3e88
Jeff <jeffcrockett03@gmail.com>
2022-07-04 21:16:50 -0400

    added jtree.py

---
+++ jtree.py
@@ -0,0 +1,11 @@
+from main import *
+from Cell import Cell as C
+from Row import Row as R
+from Table import Table as T
+from Col import Col
+import sys
+
+print(wt)
+print(dir(sys.stdout))
+
+#make a table where the first column is all the five letter words and the second column is all... hmmmm
