a87b0842801e9cc898c6e9b772796daf3b241693
Jeff <jeffcrockett03@gmail.com>
2022-07-06 11:38:47 -0400

    things almost work. ignore everything other than wordle.py

--- wordle.py
+++ wordle.py
@@ -5,8 +5,9 @@ f2 = open('wordleAlpha.txt', 'r')
 words = f2.read().split('\n')[:-1]
 
 def wordle(guesses, answer, words=words, allowed=[]):
-    guess = guesses[0]
-    if len(guess)==0:
+    if len(guesses) > 0:
+        guess = guesses[0]
+    if len(guesses)==0:
         return 'That\'s all folks'
     for i in range(5):
         if guess[i] == answer[i]:
@@ -17,9 +18,10 @@ def wordle(guesses, answer, words=words, allowed=[]):
             words = list(filter(lambda word: word[i] in answer and word[i] != answer[i] , words))
             print(guess[i], 'is yellow')
             allowed.append(guess[i])
+    print(f'There are {len(words)} words left')
     guesses = guesses[1:]
         # else:
         #     words = list(filter(lambda word: guess[i] not in word, words))
-    return wordle(answer, guesses=guesses, words=words, allowed=allowed)
+    return wordle(guesses, answer, words=words, allowed=allowed)
 
-print(wordle(answer='sever', guesses=sys.argv[1:]))
+print(wordle(sys.argv, 'severe'))
